e90705bc2f20bf4abae9252c961df2e4e20321ab
Durrez Ahmed <durrezahmed3@gmail.com>
2022-04-22 18:00:05 +0530

    editor page setup

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,7 +1,40 @@
+import { useState } from 'react';
+import Client from './../components/Client';
+import Editor from './../components/Editor';
+
 export default function EditorPage() {
+  const [clients, setClients] = useState([
+    { socketId: 1, username: 'Durrez A' },
+    { socketId: 2, username: 'John Doe' },
+    { socketId: 2, username: 'Jane Doe' },
+  ]);
+
   return (
-    <div>
-      <h1>Editor Page</h1>
+    // main wrap div
+    <div className='mainWrap'>
+      {/* aside div */}
+      <div className='aside'>
+        {/* aside inner div */}
+        <div className='asideInner'>
+          {/* logo div */}
+          <div className='logo'>
+            <img className='logoImage' src='/code-sync.png' alt='logo' />
+          </div>
+          <h3>Connected</h3>
+          {/* clients list div */}
+          <div className='clientsList'>
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
+
+        <button className='btn copyBtn'>Copy ROOM ID</button>
+        <button className='btn leaveBtn'>Leave</button>
+      </div>
+      <div className='editorWrap'>
+        <Editor />
+      </div>
     </div>
   );
 }
