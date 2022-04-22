9de36b579081a3a4312b20060df23bae7883c2bb
Durrez Ahmed <durrezahmed3@gmail.com>
2022-04-22 21:08:56 +0530

    setup server for app

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,8 +1,26 @@
-import { useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
+import ACTIONS from '../Actions';
 import Client from './../components/Client';
 import Editor from './../components/Editor';
+import { initSocket } from './../socket';
+import { useLocation } from 'react-router-dom';
 
 export default function EditorPage() {
+  const socketRef = useRef(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId: location.state?.roomId,
+        username: location.state?.username,
+      });
+    };
+
+    init();
+  }, []);
+
   const [clients, setClients] = useState([
     { socketId: 1, username: 'Durrez A' },
     { socketId: 2, username: 'John Doe' },
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,6 +25,7 @@ export default function Home() {
     // Redirect
     navigate(`/editor/${roomId}`, {
       state: {
+        roomId,
         username,
       },
     });
