0dd13fec2bd1110d46ee99bd426302a4f4919885
Durrez Ahmed <durrezahmed3@gmail.com>
2022-04-22 22:16:36 +0530

    ws error handling

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,31 +1,60 @@
 import { useRef, useState, useEffect } from 'react';
+import toast from 'react-hot-toast';
 import ACTIONS from '../Actions';
 import Client from './../components/Client';
 import Editor from './../components/Editor';
 import { initSocket } from './../socket';
-import { useLocation } from 'react-router-dom';
+import {
+  useLocation,
+  useNavigate,
+  useParams,
+  Navigate,
+} from 'react-router-dom';
 
 export default function EditorPage() {
   const socketRef = useRef(null);
   const location = useLocation();
+  const { roomId } = useParams();
+  const reactNavigator = useNavigate();
+  const [clients, setClients] = useState([]);
 
   useEffect(() => {
     const init = async () => {
       socketRef.current = await initSocket();
+
+      function handleErrors(e) {
+        console.log('socket error', e);
+        toast.error('Socket connection failed, try again later.');
+        reactNavigator('/');
+      }
+
+      socketRef.current.on('connect_error', (err) => handleErrors(err));
+      socketRef.current.on('connect_failed', (err) => handleErrors(err));
+
       socketRef.current.emit(ACTIONS.JOIN, {
-        roomId: location.state?.roomId,
+        roomId,
         username: location.state?.username,
       });
+
+      // Listening for joined event
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+            console.log(`${username} joined`);
+          }
+          setClients(clients);
+        }
+      );
     };
 
     init();
   }, []);
 
-  const [clients, setClients] = useState([
-    { socketId: 1, username: 'Durrez A' },
-    { socketId: 2, username: 'John Doe' },
-    { socketId: 3, username: 'Jane Doe' },
-  ]);
+  if (!location.state) {
+    return <Navigate to='/' />;
+  }
 
   return (
     // main wrap div
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,7 +25,6 @@ export default function Home() {
     // Redirect
     navigate(`/editor/${roomId}`, {
       state: {
-        roomId,
         username,
       },
     });
